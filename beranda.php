805689582eedf581bdd8cd5729dd62eee59465f5
Herpendi79 <119275254+Herpendi79@users.noreply.github.com>
2024-06-26 13:07:54 +0800

    Update beranda.php

--- beranda.php
+++ beranda.php
@@ -5,6 +5,106 @@
  */
 class Akreditasi
 {
-    // Kode kelas di sini
-}
+    /**
+     * Nomor perguruan tinggi.
+     *
+     * @var string
+     */
+    private $nomorPerguruanTinggi;
 
+    /**
+     * Nama perguruan tinggi.
+     *
+     * @var string
+     */
+    private $namaPerguruanTinggi;
+
+    /**
+     * Nilai akhir dalam bentuk angka.
+     *
+     * @var int
+     */
+    private $nilaiAkhirAngka;
+
+    /**
+     * Nilai akhir dalam bentuk huruf.
+     *
+     * @var string
+     */
+    private $nilaiAkhirHuruf;
+
+    /**
+     * Tanggal dan waktu penyimpanan.
+     *
+     * @var string
+     */
+    private $tanggalJamSimpan;
+
+    /**
+     * Constructor untuk kelas Akreditasi.
+     *
+     * @param string $nomorPerguruanTinggi
+     * @param string $namaPerguruanTinggi
+     * @param int $nilaiAkhirAngka
+     * @param string $nilaiAkhirHuruf
+     * @param string $tanggalJamSimpan
+     */
+    public function __construct($nomorPerguruanTinggi, $namaPerguruanTinggi, $nilaiAkhirAngka, $nilaiAkhirHuruf, $tanggalJamSimpan)
+    {
+        $this->nomorPerguruanTinggi = $nomorPerguruanTinggi;
+        $this->namaPerguruanTinggi = $namaPerguruanTinggi;
+        $this->nilaiAkhirAngka = $nilaiAkhirAngka;
+        $this->nilaiAkhirHuruf = $nilaiAkhirHuruf;
+        $this->tanggalJamSimpan = $tanggalJamSimpan;
+    }
+
+    /**
+     * Mendapatkan nomor perguruan tinggi.
+     *
+     * @return string
+     */
+    public function getNomorPerguruanTinggi()
+    {
+        return $this->nomorPerguruanTinggi;
+    }
+
+    /**
+     * Mendapatkan nama perguruan tinggi.
+     *
+     * @return string
+     */
+    public function getNamaPerguruanTinggi()
+    {
+        return $this->namaPerguruanTinggi;
+    }
+
+    /**
+     * Mendapatkan nilai akhir dalam bentuk angka.
+     *
+     * @return int
+     */
+    public function getNilaiAkhirAngka()
+    {
+        return $this->nilaiAkhirAngka;
+    }
+
+    /**
+     * Mendapatkan nilai akhir dalam bentuk huruf.
+     *
+     * @return string
+     */
+    public function getNilaiAkhirHuruf()
+    {
+        return $this->nilaiAkhirHuruf;
+    }
+
+    /**
+     * Mendapatkan tanggal dan waktu penyimpanan.
+     *
+     * @return string
+     */
+    public function getTanggalJamSimpan()
+    {
+        return $this->tanggalJamSimpan;
+    }
+}
